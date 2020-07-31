ecc370e4174687970283321ef1779e4ec6f2e0c9
Alessandro Spisso <5341363+ilGianfri@users.noreply.github.com>
2020-07-31 15:40:06 +0200

    Save settings in temp path instead of System32

--- SamFirm/Settings.cs
+++ SamFirm/Settings.cs
@@ -6,8 +6,11 @@ namespace SamFirm
 {
     internal static class Settings
     {
-        //설정파일의 이름을 가진 상수
-        private const string settingsXml = "Settings.xml";
+        //Get the temp folder path 
+        private static string GetSettingsPath()
+        {
+            return Path.GetTempPath() + @"\Settings.xml";
+        }
 
         //설정파일을 만드는 메소드
         private static void GenerateSettings()
@@ -20,19 +23,20 @@ namespace SamFirm
                     "\t<BinaryNature>False</BinaryNature>\r\n" +
                     "\t<AutoDecrypt>True</AutoDecrypt>\r\n" +
                 "</SamFirm>";
-            File.WriteAllText(settingsXml, default_contents);
+            File.WriteAllText(GetSettingsPath(), default_contents);
         }
 
         //설정파일을 읽는 메소드
         public static string ReadSetting(string element)
         {
+            string path = GetSettingsPath();
             try
             {
-                if (!File.Exists(settingsXml))
+                if (!File.Exists(path))
                 {
                     GenerateSettings();
                 }
-                return XDocument.Load(settingsXml).Element("SamFirm").Element(element).Value;
+                return XDocument.Load(path).Element("SamFirm").Element(element).Value;
             }
             catch (Exception exception)
             {
@@ -44,11 +48,13 @@ namespace SamFirm
         //설정파일을 쓰는 메소드
         public static void SetSetting(string element, string value)
         {
-            if (!File.Exists(settingsXml))
+            string path = GetSettingsPath();
+
+            if (!File.Exists(path))
             {
                 GenerateSettings();
             }
-            XDocument document = XDocument.Load(settingsXml);
+            XDocument document = XDocument.Load(path);
             XElement element2 = document.Element("SamFirm").Element(element);
             if (element2 == null)
             {
@@ -58,7 +64,7 @@ namespace SamFirm
             {
                 element2.Value = value;
             }
-            document.Save(settingsXml);
+            document.Save(path);
         }
     }
 }
